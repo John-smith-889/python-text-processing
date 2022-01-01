b64a0e552d3a3fbbb2efb10541161bd248e99e8f
Smith <bartlomiejczajewski@gmail.com>
2022-01-01 23:59:33 +0100

    Add string partitioning

--- python-text-processing.py
+++ python-text-processing.py
@@ -13,3 +13,16 @@ line = 'lala@lalala'
 line = re.sub('@', '', line)
 
 
+
+#==========================================#
+# Cut string in place of pattern occurance #
+#==========================================#
+
+line = 'lala@lalala'
+
+# Divide string into parts. String is divided by first occurance of '@' string
+line.partition('@')
+
+# get part of the string until '@'
+line.partition('@')[0]
+
