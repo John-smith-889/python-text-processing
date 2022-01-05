506f9cd9cb69d5ce8af3a44609cfc729446e6abf
Smith <bartlomiejczajewski@gmail.com>
2022-01-05 22:30:04 +0100

    Update strings processing

--- python-text-processing.py
+++ python-text-processing.py
@@ -13,16 +13,26 @@ line = 'lala@lalala'
 line = re.sub('@', '', line)
 
 
-
-#==========================================#
-# Cut string in place of pattern occurance #
-#==========================================#
+#===========================================#
+# Cut string in place of pattern occurrence #
+#===========================================#
 
 line = 'lala@lalala'
 
-# Divide string into parts. String is divided by first occurance of '@' string
+# Divide string into parts. String is divided by first occurrence of '@' string
 line.partition('@')
 
 # get part of the string until '@'
 line.partition('@')[0]
+
+
+#====================================#
+# Check if string contains substring #
+#====================================#
+
+'one' in 'one two three'
+
+
+'one' in 'one two three' in 'one two three four'
+
 
